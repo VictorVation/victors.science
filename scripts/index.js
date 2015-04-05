6847c6e12b15f470d62abf9c4c598ec7d29d5d53
Victor Szeto <me@victorszeto.com>
2015-04-05 10:54:14 -0400

    birth

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+particleground(document.getElementById('container'), {
+  density: 25000,
+  minSpeedX: 0.4,
+  minSpeedY: 0.4,
+  maxSpeedX: 1.0,
+  maxSpeedY: 1.0,
+  parallaxMultiplier: 3,
+  lineColor: '#777'
+});
